116af293e0b44ea76ca1e65d8bc1ceaee4b7c321
Tuan Do <minhtuando707@gmail.com>
2026-04-04 17:49:28 -1000

    notes-2 finished in 29 minutes.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import { Col, Row } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
+import { Contact } from '@prisma/client';
 import { adminProtectedPage } from '@/lib/page-protection';
 import { auth } from '@/lib/auth';
 import ContactCardAdmin from '@/components/ContactCardAdmin';
@@ -12,7 +13,14 @@ const AdminPage = async () => {
     } | null,
   );
 
-  const contacts = await prisma.contact.findMany({});
+  const contacts: Contact[] = await prisma.contact.findMany({
+    include: { notes: true },
+  });
+  const notes = await prisma.note.findMany({
+    where: {
+      owner: session?.user!.email ? session.user.email : undefined,
+    },
+  });
 
   return (
     <main>
@@ -20,7 +28,9 @@ const AdminPage = async () => {
       <Row xs={1} md={2} lg={3} className="g-4">    
         {contacts.map((contact, idx) => (
           <Col key={idx}>
-            <ContactCardAdmin {...contact} />
+            <ContactCardAdmin contact={contact} 
+                              notes={notes.filter(note => note.contactId === contact.id)} />
+
           </Col>
         ))}
       </Row>
